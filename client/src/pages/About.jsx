10baad1871e462f85b365ace35686b5510b09a7f
ferid aqa <fer2004id@gmail.com>
2023-12-25 01:36:36 +0400

    about add catdirilma

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import About_hero from '../component/about_hero'
 import About_10k from '../component/about_10k'
+import Catdirilma from '../component/catdirilma'
 
 function About() {
   return (
     <div >
      <About_hero/>
      <About_10k/>
+     <Catdirilma/>
     </div>
   )
 }
