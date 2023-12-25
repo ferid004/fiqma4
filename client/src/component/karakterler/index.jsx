78cf5d042bc1861e1ac881b4a51328882ec744ca
ferid aqa <fer2004id@gmail.com>
2023-12-25 20:23:07 +0400

    unide yazmışdım siniflər dəyişdi deyə pull eliyə bildədim onu indi push edirəm

---
+++ client/src/component/karakterler/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './index.scss'
+import Karakter from '../karakter'
+
+function Karakterler() {
+  return (
+    <div id='karakterler'>
+        <Karakter/>
+        <Karakter/>
+        <Karakter/>
+
+    </div>
+  )
+}
+
+export default Karakterler
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import About_hero from '../component/about_hero'
 import About_10k from '../component/about_10k'
 import Catdirilma from '../component/catdirilma'
+import Karakterler from '../component/karakterler'
 
 function About() {
   return (
     <div >
      <About_hero/>
      <About_10k/>
+     <Karakterler/>
      <Catdirilma/>
     </div>
   )
